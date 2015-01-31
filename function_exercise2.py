7d3a32a4b2a824961f8fe852e9ee860a7aaf02a2
Manuel Blank <mblank@dundee.ac.uk>
2015-01-31 19:26:33 +0100

    150131

--- function_exercise2.py
+++ function_exercise2.py
@@ -1,6 +1,6 @@
 from __future__ import division
-def get_percentage(sequence, amino_acid):
+def get_percentage(sequence, amino_acids=['A','I','L','M','F','W','Y','V']):
     length = len(sequence)
-    amino_acid_count = sequence.count(amino_acid.upper())
+    amino_acid_count = sequence.count(amino_acids)
     percentage = amino_acid_count / length
     return round(percentage, 2)
