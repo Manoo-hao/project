438eb67113ac4dbbf1e5e8f11e6451969a1a62e0
davidmam <d.m.a.martin@dundee.ac.uk>
2015-01-31 20:19:04 +0000

    comments

--- function_exercise2.py
+++ function_exercise2.py
@@ -1,6 +1,9 @@
 from __future__ import division
 def get_percentage(sequence, amino_acids=['A','I','L','M','F','W','Y','V']):
     length = len(sequence)
+    #DMAM 30/1 amino_acis is a list. You have to count each element in the list separately
     amino_acid_count = sequence.count(amino_acids)
+    #set amino_acid count to 0
+    #loop through amino_acids, adding th ecount for each amino acid to amino_acid_count
     percentage = amino_acid_count / length
     return round(percentage, 2)
