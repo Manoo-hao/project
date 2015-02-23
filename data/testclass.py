47c4404710ab4bf6cf27164b13605b4dafd01731
Manuel Blank <mblank@dundee.ac.uk>
2015-02-23 22:12:44 +0000

    minor changes

---
+++ data/testclass.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import MySQLdb
+#Incomplete outline script for a database interacting class to represent a gene.
+
+class DBHandler():
+    '''The static database connection - avoids overuse of resources'''
+    connection=None
+    dbname='mblank'
+    dbuser='mblank'
+    dbpassword='PRXUyh03'
+
+    def __init__(self):
+        '''initialising database'''
+        if DBHandler.connection == None:
+            DBHandler.connection = MySQLdb.connect(db=DBHandler.dbname, user=DBHandler.dbuser, passwd=DBHandler.dbpassword)
+            #Testing if database is connected and establish connection in case it wasn't.
+
+    def cursor(self):
+        return DBHandler.connection.cursor()
+
+class Genes():
+    '''A class that describes an individual gene'''
+    Gene_Symbol=''
+    Gene_Title=''
+    Gene_ID=''
+    probelist=[] #Each Gene can have several probes
+
+    def __init__(self,Gene_ID):
+        '''Init method for Gene'''
+        db=DBHandler()
+        self.Gene_ID=Gene_ID
+        cursor=db.cursor()
+        sql='SELECT Gene_ID, Gene_Title, Gene_Symbol FROM Genes WHERE Gene_ID=%s'
+        cursor.execute(sql,(Gene_ID,)) #query database #get result and populate the class fields.
+        result=cursor.fetchone()
+        self.Gene_Title =result[0]
+        self.Gene_Symbol=result[1]
+        return result
+
+print("Done!")
