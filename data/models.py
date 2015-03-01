749dd1abaec54476737adfc61033222c37ca420a
Manoo-hao <mblank@dundee.ac.uk>
2015-03-01 22:52:25 +0000

    Update models.py

--- data/models.py
+++ data/models.py
@@ -2,6 +2,9 @@
 import MySQLdb
 #Incomplete outline script for a database interacting class to represent a gene.
 
+#This file is the intermediate between an HTML form an an SQL query. If it was functional, I would be incorporated
+#in the python CGI script. Since this script is currently not functional, the queries were hardcoded into the CGI script
+
 class DBHandler():
     '''The static database connection - avoids overuse of resources'''
     connection=None
@@ -33,8 +36,8 @@ class Genes():
         sql='SELECT Gene_Title, Gene_Symbol from Genes where Gene_ID=%s'
         cursor.execute(sql,(Gene_ID,)) #query database #get result and populate the class fields.
         result=cursor.fetchone()
-        self.Gene_Title =result[0]
-        self.Gene_Symbol=result[1]
+        self.Gene_Title =result[0] #returns array for fetchone, for fetchall returns array of arrays, hence change to [0][0]
+        self.Gene_Symbol=result[1] #returns array for fetchone, for fetchall returns array of arrays, hence change to [0][1]
 
         #now fetch the probes..
         probesql='SELECT Gene_ID from Probes where Gene_ID=%s'
