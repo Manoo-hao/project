9863704f014cf12aee87e1e4a5f84fa1b1ba1147
Manoo-hao <mblank@dundee.ac.uk>
2015-03-01 22:56:32 +0000

    Update msscript.py

--- data/msscript.py
+++ data/msscript.py
@@ -3,7 +3,4 @@ samples="GSM567686,GSM567687,GSM567676,GSM567677,GSM567666,GSM567667".split(",")
 experiment="in vitro planktonic (control)"
 for s in samples:
    fh.write("%s\t%s\n"%(s,experiment))
-
-
-
-
+#Attempt to retrieve experimental data from the header of the GEO dataset with a sort of mini-parser.
